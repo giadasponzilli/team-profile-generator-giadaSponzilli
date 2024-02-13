6fa60b173e06a2ac617365323c4422d35d03232c
Giada Sponzilli <sponzilli.g@gmail.com>
2024-02-13 14:27:53 +0000

    added index.js functions and changed version of inquirer and jest

--- index.js
+++ index.js
@@ -10,6 +10,142 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+
+const teamMembers = [{}];
+
+function managerPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: "Manager's name",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Manager's employee ID",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Manager's email address",
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Manager's office number",
+      },
+    ])
+    .then((answer) => {
+      const manager = new Manager(answer.name, answer.id, answer.email, answer.officeNumber);
+      teamMembers.push(manager);
+      promptMenu();
+    });
+}
+
+function engineerPrompt() {
+  inquirer
+    .prompt(
+
+      {
+        type: 'input',
+        name: 'name',
+        message: "Engineer's name",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Engineer's ID",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Engineer's email address",
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: "Engineer's GitHub username",
+      },
+    )
+    .then((answer) => {
+      const engineer = new Engineer(answer.name, answer.id, answer.email, answer.github);
+      teamMembers.push(engineer)
+      promptMenu();
+    })
+}
+
+function internPrompt() {
+  inquirer
+    .prompt(
+      {
+        type: 'input',
+        name: 'name',
+        message: "Intern's name",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "Intern's ID",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "Intern's email address",
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: "Intern's school",
+      },
+    )
+    .then((answer) => {
+      const intern = new Intern(answer.name, answer.id, answer.email, answer.school);
+      teamMembers.push(intern);
+      promptMenu();
+    })
+}
+
+function promptMenu() {
+  inquirer
+    .prompt(
+      {
+        type: 'list',
+        name: 'menu',
+        message: 'What would you like to do next?',
+        choices: [
+          'Add an engineer',
+          'Add an intern',
+          'Finish building the team',
+        ]
+      },
+    )
+    .then((answers) => {
+      switch (answers.menu) {
+        case 'Add an engineer':
+          engineerPrompt();
+          break;
+        case 'Add an intern':
+          internPrompt();
+          break;
+        case 'Finish building the team':
+          generateHTML();
+          break;
+      }
+    });
+}
+
+
+function generateHTML() {
+  fs.writeFile('../output/team.html', render(teamMembers), (err) => {
+    if (err) throw err;
+    console.log('Successfully wrote to team.html');
+  });
+}
+
+managerPrompt();
